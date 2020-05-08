169642aa535b19d94795bd1f6a403db3ead758b1
viafanasyev <voafanasev@edu.hse.ru>
2020-05-08 22:41:07 +0300

    Added hotkeys hints

---
+++ src/components/HintBox/HintBox.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import classnames from "classnames/bind";
+import styles from "./HintBox.module.scss";
+import Draggable from 'react-draggable';
+
+const cx = classnames.bind(styles);
+
+class HintBox extends React.Component {
+    state = {
+        hidden: false
+    };
+
+    collapse = () => {
+        this.setState({ hidden: !this.state.hidden });
+        // FIXME: Fix unfolding in the bottom of the screen
+    };
+
+    render() {
+        return (
+            <Draggable grid={[4, 4]} bounds={"body"}>
+                <div className={cx("hint-box", {[`hint-box-hidden`]: this.state.hidden})}>
+                    <div
+                        className={cx("close-button")}
+                        onClick={() => this.collapse()}>
+                        &#x2715;
+                    </div>
+                    <div className={cx("text")}>
+                        <p>
+                            Горячие клавиши:
+                        </p>
+                        <ul>
+                            <li>Ctrl + G - сгенерировать случайные рёбра</li>
+                            <li>Ctrl + Alt + C - удалить всё</li>
+                        </ul>
+                    </div>
+                </div>
+            </Draggable>
+        );
+    }
+}
+
+export default HintBox;
